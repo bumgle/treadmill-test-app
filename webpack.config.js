12b8c4e331e3bdbe71796319aa0d580571d00b1d
makarenko <makarenko0086@gmail.com>
2019-04-24 12:34:34 +0300

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
-const mqpacker = require('css-mqpacker')
-const webpack = require('webpack')
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const mqpacker = require("css-mqpacker");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    path: path.join(__dirname, '/build'),
-    filename: '[name].[hash].js',
-    publicPath: '/'
+    path: path.join(__dirname, "/build"),
+    filename: "[name].[hash].js",
+    publicPath: "/"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              publicPath: 'fonts',
-              outputPath: 'fonts'
+              name: "[name].[ext]",
+              publicPath: "fonts",
+              outputPath: "fonts"
             }
           }
         ]
@@ -39,23 +39,23 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: path.join(__dirname, '/build')
+              publicPath: path.join(__dirname, "/build")
             }
           },
           {
-            loader: 'css-loader'
+            loader: "css-loader"
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               plugins: [
                 autoprefixer({
-                  browsers: ['ie >= 11', 'last 5 version']
+                  browsers: ["ie >= 11", "last 5 version"]
                 }),
                 mqpacker({ sort: true }),
                 cssnano({
                   preset: [
-                    'default',
+                    "default",
                     {
                       discardComments: {
                         removeAll: true
@@ -67,14 +67,14 @@ module.exports = {
             }
           },
           {
-            loader: 'sass-loader'
+            loader: "sass-loader"
           },
           {
-            loader: 'sass-resources-loader',
+            loader: "sass-resources-loader",
             options: {
               resources: [
-                './src/config/styles/variables.scss',
-                './src/config/styles/mixins.scss'
+                "./src/config/styles/variables.scss",
+                "./src/config/styles/mixins.scss"
               ]
             }
           }
@@ -82,34 +82,34 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif)$/,
-        use: ['file-loader']
+        use: ["file-loader"]
       },
       {
         test: /\.svg$/,
-        use: ['svg-url-loader']
+        use: ["svg-url-loader"]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: "./src/index.html"
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: "styles.css"
     }),
     new webpack.HashedModuleIdsPlugin(),
-    /* new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-    }), */
-    new CopyPlugin([{ from: './src/static/images', to: 'images' }])
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": '"production"'
+    }),
+    new CopyPlugin([{ from: "./src/static/images", to: "images" }])
   ],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       maxInitialRequests: Infinity,
       minSize: 0,
       cacheGroups: {
@@ -118,27 +118,27 @@ module.exports = {
           name(module) {
             const packageName = module.context.match(
               /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1]
-            return `npm.${packageName.replace('@', '')}`
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
           }
         }
       }
-    }
-    /* minimizer: [
+    },
+    minimizer: [
       new UglifyJsPlugin({
         cache: true,
         parallel: true,
         uglifyOptions: {
           compress: false,
           ecma: 6,
-          mangle: true,
-        },
-      }),
-    ], */
+          mangle: true
+        }
+      })
+    ]
   },
   devServer: {
     port: 3000,
     historyApiFallback: true,
-    contentBase: path.join(__dirname, 'build')
+    contentBase: path.join(__dirname, "build")
   }
-}
+};
